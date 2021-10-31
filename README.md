495b3e84728841b43b3e8e7a0e9f23ccd5779029
fimas <tomas.landberg@gmail.com>
2021-10-31 17:07:42 +0100

    Fix code layout issues.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # DnD Converter
 
-Run from terminal with `$ python3 gui.py` for the GUI version, or `$ python3 main.py fileA.xml fileB.csv` to run only from the terminal.
-
 Converts an XML file produced by program A to a CSV file to be imported by program B.
+
+Run from terminal with `$ python3 gui.py` for the GUI version, or `$ python3 main.py fileA.xml fileB.csv` to run only from the terminal.
 
 ![Screenshot](dnd_converter.png)
--- gui.py
+++ gui.py
@@ -26,29 +26,59 @@ class GUI(tkinter.Tk):
         self.input_frame = tkinter.LabelFrame(self, text='Input file')
         self.input_frame.grid(row=0, column=0, padx=2, pady=(2,0))
 
-        self.input_box = ttk.Entry(self.input_frame, width=33, textvariable=self.input_file)
+        self.input_box = ttk.Entry(
+            self.input_frame, 
+            width=33, 
+            textvariable=self.input_file
+        )
         self.input_box.grid(row=0, column=0, padx=2, pady=(2,0))
 
-        self.input_browse_button = ttk.Button(self.input_frame, 
+        self.input_browse_button = ttk.Button(
+            self.input_frame, 
             text='Browse...', 
-            command=lambda: self.input_file.set(filedialog.askopenfilename(filetypes=[("eXtensible Markup Language", "*.xml")])))
+            command=lambda: self.input_file.set(
+                filedialog.askopenfilename(
+                    filetypes=[("eXtensible Markup Language", "*.xml")]
+                )
+            )
+        )
         self.input_browse_button.grid(row=0, column=1, padx=2, pady=(2,0))
 
         self.output_frame = tkinter.LabelFrame(self, text='Output file')
         self.output_frame.grid(row=1, column=0, padx=2, pady=(2,0))
 
-        self.output_box = ttk.Entry(self.output_frame, width=33, textvariable=self.output_file)
+        self.output_box = ttk.Entry(
+            self.output_frame, 
+            width=33, 
+            textvariable=self.output_file
+        )
         self.output_box.grid(row=0, column=0, padx=2, pady=(2,0))
 
-        self.output_browse_button = ttk.Button(self.output_frame, 
+        self.output_browse_button = ttk.Button(
+            self.output_frame, 
             text='Browse...', 
-            command=lambda: self.output_file.set(filedialog.asksaveasfilename(filetypes=[("Comma-separated values", "*.csv")])))
+            command=lambda: self.output_file.set(
+                filedialog.asksaveasfilename(
+                    filetypes=[("Comma-separated values", "*.csv")]
+                )
+            )
+        )
         self.output_browse_button.grid(row=0, column=1, padx=2, pady=(2,0))
 
-        self.convert_button = ttk.Button(self, text='Convert', command=self.convert_command)
+        self.convert_button = ttk.Button(
+            self, 
+            text='Convert', 
+            command=self.convert_command
+        )
         self.convert_button.grid(row=2, column=0, padx=2, pady=5)
 
-        self.statusbar = tkinter.Label(self, textvariable=self.statusbar_text, bd=1, relief=tkinter.SUNKEN, anchor=tkinter.W)
+        self.statusbar = tkinter.Label(
+            self, 
+            textvariable=self.statusbar_text, 
+            bd=1, 
+            relief=tkinter.SUNKEN, 
+            anchor=tkinter.W
+        )
         self.statusbar.grid(row=3, column=0, columnspan=2, sticky='sew')
 
     def convert_command(self):
